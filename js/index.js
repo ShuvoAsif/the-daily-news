37deffd4056e9675b8156e1b28741e54da2869e2
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-09-03 10:02:52 +0600

    new

--- js/index.js
+++ js/index.js
@@ -14,11 +14,9 @@ const loadCategories = async () => {
 const displayCategories = categories => {
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
-        console.log(category)
-
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
-        <a class="btn btn-outline-success fs-5 mx-1" href="https://openapi.programming-hero.com/api/news/${category.category_id}">${category.category_name}</a>
+        <a class="btn btn-outline-success fs-6 mx-1" href="https://openapi.programming-hero.com/api/news/${category.category_id}">${category.category_name}</a>
         `;
         categoriesContainer.appendChild(categoryDiv);
     })
@@ -26,3 +24,42 @@ const displayCategories = categories => {
 
 loadCategories();
 
+//-------------------------------------------
+
+
+
+const loadNews = async id => {
+    const url = `https://openapi.programming-hero.com/api/news/${_id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNews(data.data[0]);
+}
+
+const displayNews = news => {
+    console.log(displayNews);
+    const newsContainer = document.getElementById('news-container');
+    const newsDiv = document.createElement('div');
+    newsDiv.innerHTML = `
+         <div class="card mb-3 mx-auto" style="max-width: 1000px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${news.title}</h5>
+        <p class="card-text">${news.details.slice(0, 300)}...</p>
+        <div class="d-flex  justify-content-between">
+        <div><img src="${news.author.img}" style="height: 55px;" class="img-fluid rounded-circle rounded-5" alt=""></div>
+        <div><P>${news.author.name}</P></div>
+        <div> <a class="btn btn-outline-success fs-6 mx-1" href="https://">Details</a></div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</div>`;
+    newsContainer.appendChild(newsDiv);
+}
+
+loadNews();
